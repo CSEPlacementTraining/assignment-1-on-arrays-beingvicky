6d255ca43d8658e93194fa09553341fbbcf9d728
Vikas Sampath <vikassampath1@gmail.com>
2026-03-09 11:33:35 +0000

    assignment1 done

--- Assignment1.java
+++ Assignment1.java
@@ -1,33 +1,31 @@
 class Assignment1 {
+
   public static int[] findLeapYears(int year) {
+
     int[] result = new int[15];
     int count = 0;
     int currentYear = year;
+
     while (count < 15) {
-      if (isLeapYear(currentYear)) {
+
+      if ((currentYear % 4 == 0 && currentYear % 100 != 0) || (currentYear % 400 == 0)) {
         result[count] = currentYear;
         count++;
       }
+
       currentYear++;
     }
+
     return result;
   }
-  private static boolean isLeapYear(int year) {
-    if (year % 4 == 0) {
-      if (year % 100 == 0) {
-        return year % 400 == 0;
-      } else {
-        return true;
-      }
-    } else {
-      return false;
-    }
-  }
-  
+
   public static void main(String[] args) {
+
     int year = 2000;
     int[] leapYears;
+
     leapYears = findLeapYears(year);
+
     for (int index = 0; index < leapYears.length; index++) {
       System.out.println(leapYears[index]);
     }
